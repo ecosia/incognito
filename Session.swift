31f2a7ee8179057bc09e92f68419352eb290a9c9
vauxhall <zero.griffin@gmail.com>
2020-06-25 17:13:38 +0200

    Update environment object

--- Session.swift
+++ Session.swift
@@ -2,9 +2,16 @@ import SwiftUI
 
 final class Session: ObservableObject {
     @Published var barOffset = CGFloat()
+    
+    var keyboardHeight = CGFloat() {
+        didSet {
+             barOffset += oldValue - keyboardHeight
+        }
+    }
+    
     let barHeight = CGFloat(100)
     
     func offset(_ height: CGFloat, insets: CGFloat) {
-        barOffset = height - insets - barHeight
+        barOffset = height - insets - barHeight - keyboardHeight
     }
 }
