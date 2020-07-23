1a012bc04c746cc8d1efcde6035e7b6f87a91a5c
vauxhall <zero.griffin@gmail.com>
2020-07-23 14:00:27 +0200

    Animate options

--- Bar.swift
+++ Bar.swift
@@ -3,31 +3,54 @@ import SwiftUI
 struct Bar: View {
 //    @State private var text = ""
     @State private var hide = true
+    @State private var tabsY = CGFloat()
+    @State private var menuY = CGFloat()
     
     var body: some View {
         VStack {
             Spacer()
             HStack {
-                if hide {
-                    Blob.Icon(icon: "magnifyingglass", action: show)
-                        .padding()
-                    Spacer()
-                } else {
-                    Blob.Icon(icon: "multiply", action: show)
-                        .padding()
-                    Blob.Icon(icon: "square.on.square", action: show)
-                        .padding()
-                    Blob.Icon(icon: "line.horizontal.3", action: show)
+                ZStack {
+                    if !hide {
+                        Blob.Icon(icon: "square.on.square", action: show)
+                            .padding()
+                            .offset(y: tabsY)
+                        Blob.Icon(icon: "line.horizontal.3", action: show)
+                            .padding()
+                            .offset(y: menuY)
+                    }
+                    Blob.Icon(icon: hide ? "magnifyingglass" : "multiply", action: show)
                         .padding()
                 }
+                Spacer()
             }
         }
     }
     
     private func show() {
-        withAnimation {
-            hide.toggle()
+        if hide {
+            withAnimation(Animation.linear(duration: 0.2)) {
+                hide = false
+            }
+            
+            withAnimation(Animation.easeOut(duration: 0.3).delay(0.1)) {
+                menuY = -75
+            }
+            
+            withAnimation(Animation.easeOut(duration: 0.5).delay(0.1)) {
+                tabsY = -150
+            }
+        } else {
+            withAnimation(Animation.easeOut(duration: 0.3)) {
+                tabsY = 0
+                menuY = 0
+            }
+            
+            withAnimation(Animation.linear(duration: 0.3).delay(0.1)) {
+                hide = true
+            }
         }
+        
     }
     
 //    var body: some View {
